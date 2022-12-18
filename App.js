90dce62565941dbcf4b4c5917c33093a4163ed88
ABDALAZARD <ABDALAZARD.github.com>
2022-12-18 04:46:03 -0300

    Padronizando

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, } from 'react-native';
 
 import styles from './styles';
 import Login from './src/pages/Login/index';
